a857d4c45c72527c37fe03f36f2061cb5233eebd
Dilnur Aliqulov <d.aliqulov@newuu.uz>
2023-11-14 22:03:59 +0500

    dilnuraliqulov

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -3,7 +3,16 @@
 
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
-
+  if else(firstOctet%2==0){
+      return "Unicast";
+  }
+  else if (firstOctet%2!=0 and firstOctet!=255){
+      return "Multicast";
+  }
+  else if(firstOctet==255){
+      return "Broadcast";
+  }
     // make use of control flow statements
     // return result;
+    return result;
 }
